09c3d96f7ef7421bc3c4131cf8477562fcfa2b42
Rekkur150 <minecraftzachary150@gmail.com>
2021-01-03 19:24:18 -0500

    Added Template
    Added templates, character, player, skill. (They still need work)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Tooltip("The character that the player will control")]
+    public Character PlayerControlledCharacter;
+
+
+    /// <summary>
+    /// Gets the player input then sends it to the player's character
+    /// </summary>
+    private void GetPlayerInput()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+
+    [Tooltip("The name of the skill")]
+    public string Name;
+
+    /// <summary>
+    /// When using or creating anything that needs to be saved use this, do not create another variable because it might lost D=
+    /// </summary>
+    [Tooltip("Data for the skill")]
+    public Dictionary<string, float> Data;
+
+    /// <summary>
+    /// This determines which character the skill occurs on.
+    /// </summary>
+    private Character CharacterToActivateOn;
+
+    /// <summary>
+    /// Updates the character to activate on 
+    /// </summary>
+    /// <param name="newCharacter">The new character</param>
+    public void ChangeCharacterToActivateOn(Character newCharacter)
+    {
+    }
+
+    /// <summary>
+    /// Activates this skill
+    /// </summary>
+    /// <returns>True if success, false if falure</returns>
+    public virtual bool Activate()
+    {
+        return new bool();
+    }
+
+
+}
